7a247d60fab8087eb39201163713306798dbf053
Ubuntu <ubuntu@ip-172-31-30-113.eu-west-1.compute.internal>
2019-06-17 15:05:53 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os # We're using os module to get the 'enviroment' variable > helps connect to Cloud9
 import json
-from flask import Flask, render_template # Importing our Flask Class. Render_Template supports writing of HTML code in python file.
+from flask import Flask, render_template, request, flash # Importing our Flask Class. Render_Template supports writing of HTML code in python file.
 
 app = Flask(__name__) # Flask convention (__name__) is our variable is called 'app'. Creating an instance & storing in a variable called 'app'
+app.secret_key = 'some secret' 
 
 @app.route("/") # Route decorator is a way of wrapping functions. Tells Flask what URL should trigger the function
 
@@ -19,8 +20,22 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
     
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name: # Matching url object to member name. 
+                member = obj
+                
+    return render_template("member.html", member=member) # Return to render the detail captured via member.html file
+    
+@app.route("/contact", methods=["GET", "POST"]) # The GET & POST arguments are set in a array. 
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received youre message!".format(request.form["name"])) # For Flash messaging - link to contact.html page
     return render_template("contact.html", page_title="Contact")
     
 @app.route("/careers")
