4c6c84994412b2a6f65ad75da713f020a63bdcd6
Ubuntu <ubuntu@ip-172-31-62-1.ec2.internal>
2020-06-18 15:34:35 +0000

    Debug chnaged to false as app deployed to Heroku.

--- run.py
+++ run.py
@@ -18,7 +18,6 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-        print(data)
     return render_template("about.html", page_title="About", company=data)
     
 @app.route("/about/<member_name>")
