fbeedf17897681f1dc63d6268acd4f1ba20484ea
Ubuntu <ubuntu@ip-172-31-62-1.ec2.internal>
2020-06-18 15:32:33 +0000

    Flask testing for index page to render a 200 status code.

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import urllib
+from flask import Flask
+from flask_testing import TestCase
+
+
+class test_home_directory_pages(TestCase):
+    """
+    Home page tested to return a positive 200 error code.
+    """
+    def test_home_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'index.html')
+        
+    print("All the tests passed")
